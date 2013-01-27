5b4b7c8f2d626daaf645d3c666e8f13ef38cd2ce
Cam Cook <cam.cook@linux.com>
2013-01-26 23:39:05 -0500

    Added CRUDAuths.csv to artifacts

--- src/main/java/org/worldbank/dataservice/CRUDOps.java
+++ src/main/java/org/worldbank/dataservice/CRUDOps.java
@@ -1,6 +1,12 @@
 package org.worldbank.dataservice;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public abstract class CRUDOps
 {
 
+	public abstract boolean create(String sql) throws ClassNotFoundException, SQLException;
+	public abstract ResultSet read(String query) throws ClassNotFoundException, SQLException;
+	public abstract int update(String updateSQL) throws ClassNotFoundException, SQLException;
 }
--- src/main/java/org/worldbank/dataservice/CreateOps.java
+++ src/main/java/org/worldbank/dataservice/CreateOps.java
@@ -1,19 +1,50 @@
 package org.worldbank.dataservice;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.worldbank.connector.Connector;
+import org.worldbank.constants.SQLConstants;
 import org.worldbank.users.UserType;
 
 public class CreateOps extends CRUDOps
 {
 	private UserType type;
+	private Connector conn;
 	
+	//String username, String password, String host, String port
 	public CreateOps(UserType type)
 	{
 		this.type = type;
+		conn = new Connector(SQLConstants.getRoot(),SQLConstants.getPassword(),SQLConstants.getHost(),SQLConstants.getPort());
 	}
 	
-	public void create(String sql)
+	@Override
+	public boolean create(String sql) throws ClassNotFoundException, SQLException
 	{
-		
+		switch(type)
+		{
+			case PROGENITOR:
+				conn.connect();
+				return conn.execute(sql);
+			case DEVELOPER:
+				break;
+				
+				
+		}
+	}
+	
+	@Override
+	public ResultSet read(String query) throws ClassNotFoundException, SQLException
+	{
+		conn.connect();
+		return conn.executeQuery(query);
+	}
+	
+	public int update(String updateSQL) throws ClassNotFoundException, SQLException
+	{
+		conn.connect();
+		return conn.executeUpdate(updateSQL);
 	}
 
 }
